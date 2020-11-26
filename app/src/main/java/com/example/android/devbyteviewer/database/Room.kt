51b2c23d0496b0b1dd69b31e77cd68522c63fb1a
Carlos <carlosacorti@hotmail.com>
2020-11-25 23:34:23 -0300

    implementing VideoDao

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,19 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("SELECT * FROM database_video_table")
+    fun getAllVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg videos: DatabaseVideo)
+
+}
