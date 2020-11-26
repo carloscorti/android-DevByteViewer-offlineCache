df2c764b473d045583d726a44c5295a528f4eba7
Carlos <carlosacorti@hotmail.com>
2020-11-25 23:33:16 -0300

    adding maping extension from NetworkVideoContainer to DatabaseModel

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -37,6 +38,7 @@ import com.squareup.moshi.JsonClass
  */
 @JsonClass(generateAdapter = true)
 data class NetworkVideoContainer(val videos: List<NetworkVideo>)
+
 
 /**
  * Videos represent a devbyte that can be played.
@@ -62,4 +64,15 @@ fun NetworkVideoContainer.asDomainModel(): List<Video> {
                 updated = it.updated,
                 thumbnail = it.thumbnail)
     }
+}
+
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo (
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail)
+    }.toTypedArray()
 }
